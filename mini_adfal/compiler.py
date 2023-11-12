f7ce679c222c8c634408490c8fbc6b8dd7f9a41b
superkim0610 <81168407+superkim0610@users.noreply.github.com>
2023-11-12 13:03:07 +0900

    compiler.py implemented

--- mini_adfal/compiler.py
+++ mini_adfal/compiler.py
@@ -1,12 +1,13 @@
 # input -> token
 def tokenizer(input):
 
-    def add_tokens(type, value):
+    def add_token(type, value):
         nonlocal tokens
         tokens.append({
             'type': type,
             'value': value
         })
+        print(f'add_token("{type}", "{value}")')
 
     def is_lc_letter(char):
         return ord('a') <= ord(char) <= ord('z')
@@ -16,30 +17,6 @@ def tokenizer(input):
     
     def is_digit(char):
         return ord('0') <= ord(char) <= ord('9')
-    
-    def is_identifier(char):
-        nonlocal current
-        if is_id_start(char):
-            value = ''
-            
-            while is_id_continue(char):
-                value += char
-                current += 1
-                if not current < len(input):
-                    break
-                char = input[current]
-            
-            # check value is keyword
-            if is_keyword(value):    
-                tokens.append({
-                    'type': 'keyword',
-                    'value': value
-                })
-            else:
-                tokens.append({
-                'type': 'identifier',
-                'value': value
-            })
 
     def is_id_start(char):
         return char == '_' or is_lc_letter(char) or is_uc_letter(char)
@@ -50,41 +27,13 @@ def tokenizer(input):
     def is_keyword(value):
         keywords = ["if", "eles", "while", "for", "continue", "break", "func", "return", "and", "or", "pass"]
         return value in keywords
-        if not is_string_literal(char):
-            is_number_literal(char)
 
-    def is_string_literal(char):
-        return char == '"'
+    def is_operator_part(char):
+        operator_parts = ["+", "-", "*", "/", "%", "<", ">", "!", "="]
+        return char in operator_parts
 
-    def is_number_literal(char):
-        return is_digit(char)
-        return
-        if is_digit(char):
-            value = ''
-            while is_digit(char):
-                value += char
-                current += 1
-                char = input[current]
-
-            tokens.apppend({
-                    'type': 'number_literal',
-                    'value': value
-                })
-            return True
-        return False
-
-    def is_operator(char):
-        nonlocal current
+    def is_operator(value):
         operators = ["+", "-", "*", "/", "%", "<", ">", "<=", ">=", "==", "!=", "="]
-        return value in operators
-        value = ''
-        next_char = ''
-        
-        # second char of operator is always "="
-        if current < len(input) and input[current+1] == '=':
-            next_char = '='
-            
-        value = char + next_char
         return value in operators
 
     def is_delimiter(char):
@@ -109,31 +58,34 @@ def tokenizer(input):
         
         # check delimiter
         if is_delimiter(char):
-            add_tokens('delimiter', char)
+            add_token('delimiter', char)
             
             current += 1
             continue
         
         # check operator
-        # if is_operator(char):
-        value = ''
-        next_char = ''
-        
-        # second char of operator is always "="
-        if current < len(input) and input[current+1] == '=':
-            next_char = '='
+        if is_operator_part(char):
+            value = ''
+            next_char = ''
             
-        value = char + next_char
-        add_tokens('operator', value)
-        current += len(value)
+            if current < len(input) and is_operator_part(input[current+1]):
+                next_char = input[current+1]
+            value = char + next_char
+
+            if is_operator(value):
+                add_token('operator', value)
+                current += len(value)
+                continue
         
         # check string literal
-        if is_string_literal(char):
+        if char == '"' or char == "'":
+            string_literal_delimiter = char
+
             value =''
             current += 1
             char = input[current]
             
-            while char != '"':
+            while char != string_literal_delimiter:
                 value += char
                 current += 1
                 if not current < len(input):
@@ -141,10 +93,11 @@ def tokenizer(input):
                 char = input[current]
             
             current += 1
-            add_tokens('string_literal', value)
+            add_token('string_literal', value)
+            continue
             
         # check number literal
-        if is_number_literal(char):
+        if is_digit(char):
             value = ''
             
             while is_digit(char):
@@ -154,29 +107,39 @@ def tokenizer(input):
                     break
                 char = input[current]
 
-            add_tokens('number_literal', value)
+            add_token('number_literal', value)
+            continue
             
         # check identifier, keyword
-        if is_identifier(char):
-            if is_id_start(char):
-                value = ''
-                
-                while is_id_continue(char):
-                    value += char
-                    current += 1
-                    if not current < len(input):
-                        break
-                    char = input[current]
-                
-                # check keyword
-                add_tokens('keyword' if is_keyword(value) else 'identifier', value)
+        if is_id_start(char):
+            value = ''
+            
+            while is_id_continue(char):
+                value += char
+                current += 1
+                if not current < len(input):
+                    break
+                char = input[current]
+            
+            # check keyword
+            add_token('keyword' if is_keyword(value) else 'identifier', value)
+            continue
+    
+    return tokens
         
         
 
 def parser(tokens):
     pass
 
-input = 'hi'
+input = '''
+a = 2 * 2 / 4
+b = "hello world"
+c = input()
+print(a, b, "'hi' says taeyun")
+boo.foo('c', c)
+'''
+
 tokens = tokenizer(input)
 ast = parser(parser)
 print(tokens)
